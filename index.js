feab3e6c84cd19cd10b91a4c4e2011da706d5c4c
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2020-05-15 15:28:49 +0200

    various

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+#!/usr/bin/env nodejs
+
 var express = require('express');
 var request = require('request')
 
 var app = express();
 
+const port = 3000;
 const cas_base = 'https://cas-test.univ-paris1.fr/cas'
 
 //app.use(function (req, res, next) { console.log(req.headers); next(); })
@@ -12,7 +15,6 @@ app.use(express.static(__dirname + '/app'))
 
 // proxy request
 const proxyToCas = function(req, res) {
-    console.log(req.path)
     request({ url: cas_base + req.path, qs: req.query }, function(casErr, casRes, casBody){
         if (casErr || casRes.statusCode !== 200) {
             console.log(casErr, casRes.statusCode)
@@ -22,9 +24,9 @@ const proxyToCas = function(req, res) {
     });
 }
 
-app.get('/serviceValidate', proxyToCas);
 app.get('/proxy', proxyToCas);
 app.get('/proxyValidate', proxyToCas);
+app.get('/serviceValidate', proxyToCas);
 
 let pgts = {}
 
@@ -33,10 +35,15 @@ app.get('/pgts', function (_req, res) {
 })
 
 app.get('/pgtCallback', function(req, res) {
-    console.log("got pgt:", req.query);
-    pgts[req.query.pgtIou] = req.query.pgtId;
+    if (req.query.pgtIou) {
+        console.log("pgtCallback called with params:", req.query);
+        pgts[req.query.pgtIou] = req.query.pgtId;
+    } else {
+        console.log("pgtCallback called with no parameters to first check url.", req.query);
+    }
     res.send("");
 });
 
-app.listen(3000);
 
+app.listen(port);
+console.log('Started on port ' + port);
